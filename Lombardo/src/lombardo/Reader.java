1004ce4141e02e2821501ad978688e017132f4d6
David Phelps <davidp@cshardware.com>
2016-03-30 15:47:05 -0500

    My latest commit...

--- Lombardo/src/lombardo/Reader.java
+++ Lombardo/src/lombardo/Reader.java
@@ -1,14 +1,8 @@
 
 package lombardo;
 
-/** A simple interface (abstraction) to be used by all derived classes that
- * need to perform read operations. Although using an interface as a super
- * class for low-level detail classes is the preferred technique for
- * implementing the Dependency Inversion Principle, you could make this an
- * abstract class and achieve the same results. Just make sure you keep
- * the readln() method abstract as well.
- * 
- * @author davidp
+/*
+Bunch of gibberish.
  */
 
 public interface Reader {
