be3dbe9448e6bb2a06d28f129254724287be5cea
David Phelps <davidp@cshardware.com>
2016-04-12 09:35:13 -0500

    Beginning of Lynda tutorial on Java

---
+++ LyndaJava/src/lyndajava/LyndaJava.java
@@ -0,0 +1,21 @@
+
+package lyndajava;
+
+import java.util.Scanner;
+
+public class LyndaJava {
+
+    public static void main(String[] args) {
+        
+        // declara a variable
+        String msg = "Hello World!";
+        
+        // implementing the keyboard scanner
+        Scanner keyboard = new Scanner(System.in);
+        
+        // print out the message
+        System.out.println(msg);
+        
+    }
+    
+}
